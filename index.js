51c8350ab75ae9def08d035ef3e0cd4d1a56eede
Jason Back <jason.back01@gmail.com>
2023-07-06 18:31:21 -0400

    made changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.removeChild(main)
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "Jason is the champion"
